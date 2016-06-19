8d23f703c58a1864404696512e78d8256c42836e
brry <berryboessenkool@hotmail.com>
2016-06-20 00:06:19 +0200

    projectPoints: zone default extracted from data, crs argument added

--- R/projectPoints.R
+++ R/projectPoints.R
@@ -8,7 +8,8 @@
 #' @seealso \code{\link{scaleBarOSM}}, \code{\link[OpenStreetMap]{projectMercator}}
 #' @keywords spatial
 #' @export
-#' @importFrom OpenStreetMap projectMercator
+#' @importFrom OpenStreetMap osm projectMercator
+#' @importFrom sp coordinates coordinates<- CRS proj4string proj4string<- spTransform
 #' @examples
 
 #' library("OpenStreetMap")
@@ -17,26 +18,29 @@
 #' plot(lat,lon)
 #' plot(projectMercator(lat,lon), main="Mercator")
 #' plot(projectPoints(lat,lon), main="UTM32")
+#' stopifnot(all(projectPoints(lat,lon, crs=osm())==projectMercator(lat,lon)))
 #'
 #' @param lat A vector of latitudes
 #' @param long A vector of longitudes
 #' @param drop Drop to lowest dimension? DEFAULT: FALSE (unlike projectMercator)
 #' @param zone UTM zone, see e.g. \url{https://upload.wikimedia.org/wikipedia/commons/e/ed/Utm-zones.jpg}. DEFAULT: 32
-#' @param proj Projection string to preject to. DEFAULT: UTM zone 32 (center Germany)
+#' @param proj Projection string to preject to. DEFAULT: UTM zone at mean(long)
+#' @param crs Coordinate Reference System Object. DEFAULT: CRS(proj)
 #'
 
 projectPoints <- function (
 lat,
 long,
 drop=FALSE,
-zone=32,
-proj=paste0("+proj=utm +zone=",zone,"+ellps=WGS84 +datum=WGS84")
+zone=mean(long)%/%6+31,
+proj=paste0("+proj=utm +zone=",zone,"+ellps=WGS84 +datum=WGS84"),
+crs=CRS(proj)
 )
 {
 df <- data.frame(long = long, lat = lat)
 coordinates(df) <- ~long + lat
 proj4string(df) <- CRS("+proj=longlat +datum=WGS84")
-df1 <- spTransform(df, CRS(proj))
+df1 <- spTransform(df, crs)
 coords <- coordinates(df1)
 colnames(coords) <- c("x", "y")
 if (drop) coords <- drop(coords)
