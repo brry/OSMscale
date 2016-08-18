ad3ac0a4968d8580c71207af084204d216e30488
brry <berryboessenkool@hotmail.com>
2016-08-18 19:36:10 +0200

    spatially related files moved here from berryFunctions

--- R/OSMscale-package.R
+++ R/OSMscale-package.R
@@ -3,6 +3,7 @@
 #' Functionality to handle and project lat-long coordinates,
 #' easily download background maps
 #' and add a correct scale bar to 'OpenStreetMap' plots in any map projection.
+#' There are some other spatially related miscellaneous functions as well.
 #'
 #' @name OSMscale-package
 #' @aliases OSMscale-package OSMscale
@@ -27,3 +28,24 @@
 #'
 NULL
 
+#' GPS recorded bike track
+#'
+#' My daily bike route, recorded with the app OSMtracker on my Samsung Galaxy S5
+#'
+#'
+#' @name biketrack
+#' @docType data
+#' @format 'data.frame':	254 obs. of  4 variables:\cr
+#' $ lon : num  13 13 13 13 13 ...\cr
+#' $ lat : num  52.4 52.4 52.4 52.4 52.4 ...\cr
+#' $ time: POSIXct, format: "2016-05-18 07:53:22" "2016-05-18 07:53:23" ...\cr
+#' $ ele : num  66 66 66 67 67 67 68 69 69 69 ....
+#' @source GPS track export from OSMtracker App
+#' @keywords datasets
+#' @examples
+#'
+#' data(biketrack)
+#' plot(biketrack[,1:2])
+#' # see equidistPoints
+#'
+NULL
---
+++ R/randomPoints.R
@@ -0,0 +1,62 @@
+#' Distanced random points
+#'
+#' Arranges points in square randomly, but with certain minimal distance to each other
+#'
+#' @return data.frame with x and y coordinates.
+#' @author Berry Boessenkool, \email{berry-b@@gmx.de}, 2011/2012
+#' @seealso \code{\link{distance}}, the package RandomFields ( \url{https://cran.r-project.org/package=RandomFields})
+#' @keywords datagen spatial
+#' @importFrom graphics plot
+#' @importFrom stats runif
+#' @importFrom berryFunctions distance
+#' @export
+#' @examples
+#'
+#' P <- randomPoints(xmin=200,xmax=700, ymin=300,ymax=680, number=60,mindist=10, asp=1)
+#' rect(xleft=200, ybottom=300, xright=700, ytop=680, col=NA, border=1)
+#'
+#' format( round(P,4), trim=FALSE)
+#'
+#' for(i in 1:10)
+#' {
+#' rp <- randomPoints(xmin=0,xmax=20, ymin=0,ymax=20, number=20, mindist=3, plot=FALSE)
+#' plot(rp, las=1, asp=1, pch=16)
+#' abline(h=0:30*2, v=0:30*2, col=8); box()
+#' for(i in 1:nrow(rp))
+#'     berryFunctions::circle(rp$x[i],rp$y[i], r=3, col=rgb(1,0,0,alpha=0.2), border=NA)
+#' }
+#'
+#' @param xmin Minimum x coordinate
+#' @param xmax Upper limit x values
+#' @param ymin Ditto for y
+#' @param ymax And yet again: Ditto.
+#' @param number How many points should be randomly + uniformly distributed
+#' @param mindist Minimum DIstance each point should have to others
+#' @param plot Plot the result? DEFAULT: TRUE
+#' @param \dots Further arguments passed to plot
+#'
+randomPoints <- function(
+xmin,
+xmax,
+ymin,
+ymax,
+number,
+mindist,
+plot=TRUE,
+...)
+{
+# target vectors for randomly distributed points:
+x <- y <- rep(NA, number)
+# first value
+x[1] <- runif(1, xmin, xmax) ; y[1] <- runif(1, ymin, ymax)
+# add number-1 points
+for(i in 2:number)
+   {x[i] <- runif(1, xmin, xmax) ; y[i] <- runif(1, ymin, ymax)
+   # if minimal distance is not kept, replace the point:
+   while(   min(distance(x[i], y[i], x[-i],y[-i]), na.rm=TRUE) < mindist  )
+   {x[i] <- runif(1, xmin, xmax) ; y[i] <- runif(1, ymin, ymax) }
+   }
+# plot result if wanted
+if(plot) plot(x,y,las=1,pch=16, ...)
+return(data.frame(x,y))
+}
