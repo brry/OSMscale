ab76b3479f943fd12b851f58ca62c2266f323789
brry <berry-b@gmx.de>
2017-04-19 19:04:47 +0200

    maxEarthDist: generalized to take any function, not just max. Can now also be applied to each point separately.

--- R/maxEarthDist.R
+++ R/maxEarthDist.R
@@ -14,23 +14,35 @@
 #' d <- read.table(header=TRUE, text="
 #'     x     y
 #' 9.19 45.73
+#' 1.12 53.12
 #' 6.55 58.13
-#' 7.71 71.44")
+#' 7.71 71.44
+#' ")
 #'
-#' plot(d, asp=1, pch=as.character(1:3))
+#' plot(d, asp=1, pch=as.character(1:4))
 #' earthDist(y,x,d, i=2)
 #' earthDist(y,x,d, i=3)
 #'
+#' maxEarthDist(y,x,d, each=FALSE)
 #' maxEarthDist(y,x,d)
+#' maxEarthDist(y,x,d, fun=min)
 #'
 #' @param lat,long,data Coordinates for \code{\link{earthDist}}
-#' @param r radius for \code{\link{earthDist}}
+#' @param r     Earth Radius for \code{\link{earthDist}}
+#' @param fun   Function to be applied. DEFAULT: \code{\link{max}}
+#' @param each  Logical: give max dist to all other points for each point?
+#'              If FALSE, will return the maximum of the complete distance matrix.
+#'              DEFAULT: TRUE
+#' @param \dots Further arguments passed to fun, like na.rm=TRUE
 #'
 maxEarthDist <- function(
 lat,
 long,
 data,
-r=6371
+r=6371,
+fun=max,
+each=TRUE,
+...
 )
 {
 if(!missing(data)) # get lat and long from data.frame
@@ -38,8 +50,9 @@ if(!missing(data)) # get lat and long from data.frame
   lat  <- getColumn(substitute(lat) , data)
   long <- getColumn(substitute(long), data)
   }
-d <- sapply(seq_along(lat), function(i) earthDist(lat,long,r=r,i=i) )
-max(d, na.rm=TRUE)
+d <- sapply(seq_along(lat), function(i) earthDist(lat,long,r=r,i=i)[-i] )
+if(!each) return(  fun(d, ...)  )   # d[upper.tri(d)]
+apply(d, 2, fun, ...)
 }
 
 
