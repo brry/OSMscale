761d90b00f84e8a3a18ee75056fb0073e8fefba8
brry <berryboessenkool@hotmail.com>
2017-06-07 10:49:32 +0200

    earthDist: along argument added

--- R/earthDist.R
+++ R/earthDist.R
@@ -25,6 +25,13 @@
 #' # install.packages("geosphere")
 #' # geosphere::distHaversine(as.matrix(d[1,2:1]), as.matrix(d[2,2:1])) / 1000
 #'
+#' # Distance along vector of points:
+#' d <- data.frame(lat=21:50, long=1:30)
+#' pointsMap(lat,long,d, zoom=2, proj=putm(1:30) )
+#' along1 <- earthDist(lat,long,d, along=TRUE)
+#' along2 <- c(0, sapply(2:nrow(d), function(i) earthDist(lat,long,data=d[i-1:0,])[2]))
+#' along1-along2 # all zero, but second version is MUCH slower for large datasets
+#'
 #'
 #' # compare with UTM distance
 #' set.seed(42)
@@ -40,17 +47,20 @@
 #'
 #'
 #' @param lat,long Latitude (North/South) and longitude (East/West) coordinates in decimal degrees
-#' @param data Optional: data.frame with the columns \code{lat} and \code{long}
-#' @param r radius of the earth. Could be given in miles. DEFAULT: 6371 (km)
-#' @param i Integer: Index element against which all coordinate pairs
-#'          are computed. DEFAULT: 1
+#' @param data     Optional: data.frame with the columns \code{lat} and \code{long}
+#' @param r        radius of the earth. Could be given in miles. DEFAULT: 6371 (km)
+#' @param i        Integer: Index element against which all coordinate pairs
+#'                 are computed. DEFAULT: 1
+#' @param along    Logical: Should distances be computed along vector of points?
+#'                 If TRUE, \code{i} is ignored. DEFAULT: FALSE
 #'
 earthDist <- function(
 lat,
 long,
 data,
 r=6371,
-i=1L
+i=1L,
+along=FALSE
 )
 {
 # Input coordinates:
@@ -68,6 +78,14 @@ y1 <-  lat[i]*pi/180
 x1 <- long[i]*pi/180
 y2 <-  lat*pi/180
 x2 <- long*pi/180
+if(along)
+{
+n <- length(lat)
+y1 <-  lat[-1]*pi/180
+x1 <- long[-1]*pi/180
+y2 <-  lat[-n]*pi/180
+x2 <- long[-n]*pi/180
+}
 # angle preparation (numerical inaccuracies may lead to 1.0000000000000002):
 cosinusangle <- sin(y1)*sin(y2) + cos(y1)*cos(y2)*cos(x1-x2)
 cosinusangle <- replace(cosinusangle, cosinusangle>1, 1)
@@ -75,8 +93,10 @@ cosinusangle <- replace(cosinusangle, cosinusangle>1, 1)
 angle <- acos( cosinusangle )
 # berryFunctions::almost.equal is too slow in vectorized settings like maxEarthDist
 tol <- sqrt(.Machine$double.eps) # equality tolerance
-samepoint <-    abs(x2-x1) < tol  &   abs(y2-y1) < tol 
+samepoint <-    abs(x2-x1) < tol  &   abs(y2-y1) < tol
 angle[samepoint] <- 0 # again, to compensate numerical inaccuracies
+# prepend zero if along is TRUE:
+if(along) angle <- c(0, angle)
 # compute great-circle-distance:
 r*angle
 }
